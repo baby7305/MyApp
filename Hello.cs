834073fc4f13de496b139ef1a3c51ef10a926b0a
baby7305 <songhaibei7305@163.com>
2020-06-10 15:33:36 +0800

    测试摄像头连通

--- Hello.cs
+++ Hello.cs
@@ -33,6 +33,17 @@ namespace MyApp
             connectTask.Wait(CancellationToken.None);
         }
 
+        public async Task<bool> test3()
+        {
+            var serverUri = new Uri("rtsp://192.168.0.5:554/h264/ch1/main/av_stream");
+            var credentials = new NetworkCredential("admin", "12345qwert");
+
+            var connectionParameters = new ConnectionParameters(serverUri, credentials);
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            return await ConnectAsync1(connectionParameters, cancellationTokenSource.Token);
+        }
+
         private static async Task ConnectAsync(ConnectionParameters connectionParameters, CancellationToken token)
         {
             try
@@ -84,6 +95,44 @@ namespace MyApp
             catch (OperationCanceledException)
             {
             }
+        }
+
+        private async Task<bool> ConnectAsync1(ConnectionParameters connectionParameters, CancellationToken token)
+        {
+            var result = false;
+            try
+            {
+                TimeSpan delay = TimeSpan.FromSeconds(5);
+
+                using (var rtspClient = new RtspClient(connectionParameters))
+                {
+                    rtspClient.FrameReceived +=
+                        (sender, frame) => Console.WriteLine($"New frame {frame.Timestamp}: {frame.GetType().Name}");
+
+                    Console.WriteLine("Connecting...");
+
+                    try
+                    {
+                        await rtspClient.ConnectAsync(token);
+                        result = true;
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        return false;
+                    }
+                    catch (RtspClientException e)
+                    {
+                        Console.WriteLine(e.ToString());
+                        await Task.Delay(delay, token);
+                        result = false;
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+
+            return result;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ namespace MyApp
         {
             var hello = new Hello();
             hello.test1();
-            hello.test2();
+            var resultTask = hello.test3();
+            // resultTask.Wait(5000);
+            var result = resultTask.Result;
+            if (result)
+            {
+                Console.WriteLine("摄像头已连接");
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
